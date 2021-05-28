d27987ca2c4afabe1b6ef318878cba91ed3241cb
mwins21 <64426103+mwins21@users.noreply.github.com>
2021-05-28 10:28:04 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,11 +94,10 @@ resource "luminate_web_access_policy" "web-access-policy" {
 }
 
 // Change for Account in SAC
-//data "luminate_identity_provider" "idp" {
-data "luminate_identity_provider" "my-identity-provider" {  
+data "luminate_identity_provider" "idp" {
   //identity_provider_name = var.luminate_idp
-  identity_provider_name = "SAC-Okta"
-  //identity_provider_name = "local"
+  //identity_provider_name = "SAC-Okta"
+  identity_provider_name = "local"
 }
 
 data "luminate_user" "users" {
